20c1031d2ebac45059c01c7aaeda9ffb95f5d78f
Peter Good <peterwgood77@gmail.com>
2026-03-29 08:32:47 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // If you are using the v4 vite plugin
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/fintech-interface-prototype/',
+  plugins: [react(), tailwindcss()],
+  base: '/fintech-interface-prototype/', // This line is CRITICAL for GitHub Pages
 })
